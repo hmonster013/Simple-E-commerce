e3f44c599aee347cda0a947c82217e05555fa947
hmonster013 <tranphuhuyit@gmail.com>
2025-01-28 23:20:11 +0700

    Restructure project: split into 3 modules - api, common, job

---
+++ ecommerce-common/src/main/java/com/de013/EcommerceCommonApplication.java
@@ -0,0 +1,13 @@
+package com.de013;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceCommonApplication.class, args);
+	}
+
+}
---
+++ ecommerce-job/src/main/java/com/de013/EcommerceJobApplication.java
@@ -0,0 +1,13 @@
+package com.de013;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceJobApplication.class, args);
+	}
+
+}
